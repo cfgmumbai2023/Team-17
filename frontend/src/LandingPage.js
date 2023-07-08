9f49516ee29063a2c9cbc705f2c9441c2594d21b
Mohit <mv4199671@gmail.com>
2023-07-09 02:15:17 +0530

    landingPage.js

---
+++ frontend/src/LandingPage.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import './LoginPage.css';
+
+function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Add your login logic here
+    console.log('Email:', email);
+    console.log('Password:', password);
+    // Reset the form fields
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div className="container-fluid login-page">
+      <div className="row">
+        <div className="col-md-6 login-bg">
+          <h2>Login</h2>
+        </div>
+        <div className="col-md-6 login-panel">
+          <h2>Login Panel</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={handleEmailChange}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={handlePasswordChange}
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
